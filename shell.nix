18aadd406fb4d73229266d78368106d3bdf6996a
Sam Nolan <samnolan555@gmail.com>
2021-05-17 11:30:54 +1000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  R-with-my-packages = pkgs.rWrapper.override { packages = with pkgs.rPackages; [ rstan ggplot2 StanHeaders ]; };
+in
+pkgs.mkShell {
+  name = "stan-env";
+  buildInputs = [ R-with-my-packages ];
+}
