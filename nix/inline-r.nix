8753bac23acb9dc1d9c44201283bb12bf82b216d
Sam Nolan <samnolan555@gmail.com>
2021-06-18 16:29:03 +1000

    Add nix folder

---
+++ nix/inline-r.nix
@@ -0,0 +1,33 @@
+{ mkDerivation, aeson, base, bytestring, containers, criterion
+, data-default-class, deepseq, directory, exceptions, filepath
+, ieee754, inline-c, lib, mtl, pretty, primitive, process
+, quickcheck-assertions, R, reflection, setenv, silently
+, singletons, strict, tasty, tasty-expected-failure, tasty-golden
+, tasty-hunit, tasty-quickcheck, template-haskell, temporary, text
+, th-lift, th-orphans, transformers, unix, vector
+}:
+mkDerivation {
+  pname = "inline-r";
+  version = "0.10.4";
+  sha256 = "b4222e49587132fc5596373558c33d9d7637b483309bff7bb1c076f8a3886e4b";
+  libraryHaskellDepends = [
+    aeson base bytestring containers data-default-class deepseq
+    exceptions inline-c mtl pretty primitive process reflection setenv
+    singletons template-haskell text th-lift th-orphans transformers
+    unix vector
+  ];
+  libraryPkgconfigDepends = [ R ];
+  testHaskellDepends = [
+    base bytestring directory filepath ieee754 mtl process
+    quickcheck-assertions silently singletons strict tasty
+    tasty-expected-failure tasty-golden tasty-hunit tasty-quickcheck
+    template-haskell temporary text unix vector
+  ];
+  benchmarkHaskellDepends = [
+    base criterion filepath primitive process singletons
+    template-haskell vector
+  ];
+  homepage = "https://tweag.github.io/HaskellR";
+  description = "Seamlessly call R from Haskell and vice versa. No FFI required.";
+  license = lib.licenses.bsd3;
+}
