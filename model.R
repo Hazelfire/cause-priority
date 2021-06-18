0d1a536195a3a384b291e4b52bfbf13a4ce17e49
Sam Nolan <samnolan555@gmail.com>
2021-06-18 15:56:49 +1000

    Add jupyter notebook

--- model.R
+++ model.R
@@ -3,54 +3,66 @@ library(tidyverse)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
+N <- 10000
+
 model <- list(donation_size=100000,
-                    discount_rate=0.04,
-                    value_of_ln_consumption = 1.44,
-                    value_of_under_5_death_averted = 117,
-                    value_of_over_5_death_averted = 83
-                    )
+              discount_rate=0.04,
+              value_of_ln_consumption = 1.44,
+              value_of_under_5_death_averted = 117,
+              value_of_over_5_death_averted = 83
+              )
+
 prior_predictive_beta <- function(params) {
-  prior_alpha = rgamma(10000, shape=2,scale = params$alpha_prior_mu)
-  prior_beta = rgamma(10000,shape = 2, scale = params$beta_prior_mu)
-  return(rbeta(10000, shape1=prior_alpha + 1, shape2=prior_beta + 1))
+  prior_alpha = rgamma(N, shape=2,scale = params$alpha_prior_scale)
+  prior_beta = rgamma(N,shape = 2, scale = params$beta_prior_scale)
+  return(rbeta(N, shape1=prior_alpha + 1, shape2=prior_beta + 1))
 }
 
 prior_predictive_normal_log <- function(params) {
-  prior_mu = rlnorm(10000, meanlog=params$mu_prior_mu,sdlog=params$mu_prior_sigma)
-  prior_sigma = rexp(10000,rate=1/params$sigma_prior_mu)
-  return(rnorm(10000, mean=prior_mu, sd=prior_sigma))
+  prior_mu = rlnorm(N, meanlog=params$mu_prior_mu,sdlog=params$mu_prior_sigma)
+  prior_sigma = rexp(N,rate=1/params$sigma_prior_mu)
+  return(rnorm(N, mean=prior_mu, sd=prior_sigma))
 }
 
 prior_predictive_poisson <- function(params) {
-  prior_mu = rexp(10000, rate=1/params$mu_prior_mu)
-  return(rpois(10000, lambda=prior_mu))
+  prior_mu = rexp(N, rate=1/params$mu_prior_mu)
+  return(rpois(N, lambda=prior_mu))
 }
 
-proportion_to_program <- 
-  list(data_size = 0,
-       y = c(),
-       alpha_prior_mu = 8,
-       beta_prior_mu = 2
-       )
-size_of_transfer_prior <- 
-  list(data_size = 0,
-       y = c(),
-       mu_prior_mu = log(1000),
-       mu_prior_sigma = 1,
-       sigma_prior_mu = 100
-       )
-household_size <-
-  list(data_size = 0,
-       y = c(),
-       mu_prior_mu = 4
-       )
+beta_distr <- function(alpha_scale, beta_scale) {
+  return(
+    list(data_size = 0,
+         y = c(),
+         alpha_prior_scale = alpha_scale,
+         beta_prior_scale = beta_scale
+         ))
+}
 
-transfers_invested <-
-  list(data_size = 0,
+normal_distr <- function(mu_prior_mu, mu_prior_sigma, sigma_prior_mu) {
+  return(list(data_size = 0,
        y = c(),
-       alpha_prior_mu = 1,
-       beta_prior_mu = 10
-       )
+       mu_prior_mu = mu_prior_mu,
+       mu_prior_sigma = mu_prior_sigma,
+       sigma_prior_mu = sigma_prior_mu
+       ))
+}
+
+poisson_distr <- function(mu_prior_mu) {
+  return(
+    list(data_size = 0,
+         y = c(),
+         mu_prior_mu = 4
+         )
+    )
+}
+
+proportion_to_program <- beta_distr(8, 2)
+size_of_transfer <- normal_distr(log(1000), 1, 100)
+household_size <- poisson_distr(4)
+transfers_invested <- beta_distr(8, 2)
+roi <- beta_distr(1, 10)
+# I need to work out 
+consumption_per_capita <- normal_distr(log(500), 1, 100)
 #fit <- stan(file = 'models/beta.stan', data = beta_model)
 
 #print(fit)
--- shell.nix
+++ shell.nix
@@ -1,13 +1,40 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  R-with-my-packages = pkgs.rWrapper.override { packages = with pkgs.rPackages; [ 
-    rstan 
-    ggplot2 
-    StanHeaders 
-    tidyverse
-  ]; };
+  jupyter = import (
+    pkgs.fetchFromGitHub {
+      owner = "tweag";
+      repo = "jupyterWith";
+      rev = "acf20ff7e3b45bd2978326e17f0de6e2f45c687f";
+      sha256 = "0i77c9wcnm0jj6czsy6vnf1f8ydzifddz2z7wz0afnpabl4kv3wn";
+      fetchSubmodules = true;
+    }) {};
+
+  my_inline_r = pkgs.haskellPackages.callPackage ./nix/inline-r.nix {};
+  iHaskell = jupyter.kernels.iHaskellWith {
+    extraIHaskellFlags = "--codemirror Haskell";  # for jupyterlab syntax highlighting
+    name = "haskell";
+    packages = p: with p; [ hvega formatting my_inline_r ];
+  };
+
+  irkernel = jupyter.kernels.iRWith {
+    # Identifier that will appear on the Jupyter interface.
+    name = "nixpkgs";
+    # Libraries to be available to the kernel.
+    packages = p: with p; [ 
+      rstan 
+      ggplot2 
+      StanHeaders 
+      tidyverse
+    ];
+    # Optional definition of `rPackages` to be used.
+    # Useful for overlaying packages.
+    rPackages = pkgs.rPackages;
+  };
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ irkernel iHaskell ];
+    };
 in
-pkgs.mkShell {
-  name = "stan-env";
-  buildInputs = [ R-with-my-packages ];
-}
+  jupyterEnvironment.env
+
